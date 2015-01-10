34eb696ad5c34021bac99a8980b8b218b3ccb17c
bibiana <bcristofol@gmail.com>
2015-01-10 20:44:16 +0000

    Game class - Should only accept rock, paper or scissors as valid options

--- lib/game.rb
+++ lib/game.rb
@@ -4,11 +4,20 @@ class Game
 
   def initialize
     @players = []
+    
   end
 
   def add!(player)
     @players << player
     raise "Only two players are allowed" if @players.length > 2
+  end
+
+  def is_valid?(choice)
+    if choice == 'rock' || choice == 'paper' || choice == 'scissors'
+      return true
+    else
+      raise "Not a valid option"
+    end
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,4 +18,21 @@ describe Game do
     game.add!(player)
     expect(lambda{ game.add!(player) }).to raise_error(RuntimeError, 'Only two players are allowed')
   end
+
+  it 'should accept rock as a valid choice' do
+    expect(game.is_valid?('rock')).to eq(true)
+  end
+
+  it 'should accept paper as a valid choice' do
+    expect(game.is_valid?('paper')).to eq(true)
+  end
+
+  it 'should accept scissors as a valid choice' do
+    expect(game.is_valid?('scissors')).to eq(true)
+  end
+
+  it 'should raise an error if the choice is not valid' do
+    expect(lambda{ game.is_valid?('banana') }).to raise_error(RuntimeError, 'Not a valid option')
+  end
+
 end
