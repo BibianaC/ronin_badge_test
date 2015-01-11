60530f243c6a8c12a1abe5e6a51da7be6faf7fc5
bibiana <bcristofol@gmail.com>
2015-01-11 16:03:14 +0000

    Game class - should raise an error if the choice is added for a player that is not in the game

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,6 @@ class Game
 
   def initialize
     @players = []
-    
   end
 
   def add!(player)
@@ -16,8 +15,41 @@ class Game
     if choice == 'rock' || choice == 'paper' || choice == 'scissors'
       return true
     else
-      raise "Not a valid option"
+      false
     end
   end
+
+  def choose(player, choice)
+    raise "#{choice} is not a valid choice" if !is_valid?(choice)
+    if @players[0] == player
+      @player1_choice = choice 
+    elsif @players[1] == player
+      @player2_choice = choice
+    else
+      raise "The player #{player.name} is not part of the game"
+    end
+  end
+
+  # def result
+  #   if @player1_choice == nil || @player2_choice == nil
+  #     return "Player choices are not set up"
+  #   elsif @player1_choice == @player2_choice
+  #     return "It's a draw"
+  #   elsif @player1_choice == "rock" && @player2_choice == "paper"
+  #     return "#{players[0].name} lose"
+  #   elsif @player1_choice == "rock" && @player2_choice == "scissors"
+  #     return "You win"
+  #   elsif @player1_choice == "paper" && @player2_choice == "rock"
+  #     return "You win"
+  #   elsif @player1_choice == "paper" && @player2_choice == "scissors"
+  #     return "You lose"
+  #   elsif @player1_choice == "scissors" && @player2_choice == "rock"
+  #     return "You lose"
+  #   elsif @player1_choice == "scissors" && @player2_choice == "paper"
+  #     return "You win"
+  #   else
+  #     return "error"
+  #   end
+  # end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,7 @@ require 'game'
 describe Game do
 	let(:game) {Game.new}
   let(:player) {Player.new('Bibiana')}
+  let(:player2) {Player.new('Serafeim')}
 
   it 'should not have players when initialize' do
     expect(game.players).to be_empty
@@ -16,7 +17,7 @@ describe Game do
   it 'should have max two players' do
     game.add!(player)
     game.add!(player)
-    expect(lambda{ game.add!(player) }).to raise_error(RuntimeError, 'Only two players are allowed')
+    expect(lambda { game.add!(player) }).to raise_error(RuntimeError, 'Only two players are allowed')
   end
 
   it 'should accept rock as a valid choice' do
@@ -32,7 +33,20 @@ describe Game do
   end
 
   it 'should raise an error if the choice is not valid' do
-    expect(lambda{ game.is_valid?('banana') }).to raise_error(RuntimeError, 'Not a valid option')
+    expect(game.is_valid?('banana')).to eq(false)
+  end
+
+  it 'should raise an error if the choice is added for a player that is not in the game' do
+    player_test = Player.new('Bibiana')
+    expect(lambda { game.choose(player_test, 'rock') }).to raise_error(RuntimeError, "The player #{player.name} is not part of the game")
+  end
+
+  xit 'should say it is a draw if the two players have the same choice' do
+    game.add!(player)
+    game.choose(player, 'rock')
+    game.add!(player2)
+    game.choose(player2, 'rock')
+    expect(game.result).to eq('It\'s a draw')
   end
 
 end
